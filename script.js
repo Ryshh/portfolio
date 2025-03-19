b486d86c2668d45262af65808e9c5216f29c6a3b
Rysh <nagymarcell60@gmail.com>
2025-03-19 09:56:02 +0100

    second

---
+++ script.js
@@ -0,0 +1,42 @@
+const url = "https://raw.githubusercontent.com/Ryshh/portfolio-json/refs/heads/main/portfolio.json"
+
+function CreateProjectCard(data)
+{
+    var temakorok = ""
+
+    data.topics.forEach(temakor => {
+        temakorok += `<li>${temakor}</li>`
+    });
+
+    return `<div class="project-container">
+                <div class="project-layout">
+                    <div class="project-img">
+                        <img src="${data.photo_url}" alt="">
+                    </div>
+                    <div class="project-description">
+                        <h3>${data.title}</h3>
+                        <hr>
+                        <p>Témakörök</p>
+                        <ul>
+                            ${temakorok}
+                        </ul>
+
+                        <div class="project-links">
+                            <a href="${data.repo_link}" target="_blank" style="text-align: right;">Github link</a>
+                            <a href="${data.site_link}" target="_blank" style="text-align: right;">Weboldal</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`
+}
+
+function Render(json)
+{
+    const container = document.getElementById("projects")
+
+    json.forEach(data => {
+        container.innerHTML += CreateProjectCard(data)
+    });
+}
+
+getData(url, Render)
